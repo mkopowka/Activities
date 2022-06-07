bbea24b64c0023a2a50a64189833f58192a5d173
Mateusz Kopówka <mkopowka@hotmail.com>
2022-06-07 23:59:18 +0200

    Styling

---
+++ client/src/features/activities/dashboard/ActivityFilters.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Calendar from "react-calendar";
+import { Header, Menu } from "semantic-ui-react";
+
+export default function ActivityFilters() {
+    return (
+        <>
+            <Menu vertical size='large' style={{ width: '100%' }}>
+                <Header icon='filter' attached color='teal' content='Filters' />
+                <Menu.Item content='All Activites' />
+                <Menu.Item content='Im going' />
+                <Menu.Item content='Im hosting' />
+            </Menu>
+            <Header />
+            <Calendar />
+        </>
+
+    )
+}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/layout/App';
 import './app/layout/styles.css';
+import 'react-calendar/dist/Calendar.css'
 import { store, StoreContext } from './app/stores/store';
 import reportWebVitals from './reportWebVitals';
 
