291b0dc52f445f5046e4f4ccec51195b1b50ea16
Mateusz Kopówka <mkopowka@hotmail.com>
2022-07-19 09:00:17 +0200

    Pages

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -11,9 +11,13 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<List<ActivityDto>>> { }
+        public class Query : IRequest<Result<PagedList<ActivityDto>>>
+        {
 
-        public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
+            public PagingParams Params { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result<PagedList<ActivityDto>>>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
@@ -23,14 +27,17 @@ namespace Application.Activities
                 _context = context;
                 _mapper = mapper;
             }
-            
-            public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
-            {
-                var activities = await _context.Activities
-                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
-                    .ToListAsync(cancellationToken);
 
-                return Result<List<ActivityDto>>.Success(activities);
+            public async Task<Result<PagedList<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var query = _context.Activities
+                    .OrderBy(d => d.Date)
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+                    .AsQueryable();
+
+                return Result<PagedList<ActivityDto>>.Success(
+                    await PagedList<ActivityDto>.CreateAsync(query, request.Params.PageNumber, request.Params.PageSize)
+                );
             }
         }
     }
