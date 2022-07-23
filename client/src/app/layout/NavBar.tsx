35322378ffc8910812ee0cb1565f79ff2301a6c4
Mateusz Kopówka <mkopowka@hotmail.com>
2022-07-23 08:23:53 +0200

    heroku deploy

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -12,7 +12,7 @@ export default observer(function NavBar() {
             <Container>
                 <Menu.Item as={NavLink} to='/' exact header>
                     <img src="/assets/logo.png" alt="logo" style={{ marginRight: '10px' }} />
-                    Reactivities
+                    Meets
                 </Menu.Item>
                 <Menu.Item as={NavLink} to='/activities' name='Activities' />
                 <Menu.Item as={NavLink} to='/errors' name='Errors' />
