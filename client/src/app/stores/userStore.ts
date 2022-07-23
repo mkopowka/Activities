94bc336b46854d7507cb81782e878c8919d50df9
Mateusz Kopówka <mkopowka@hotmail.com>
2022-07-23 14:29:53 +0200

    FB login complete

--- client/src/app/stores/userStore.ts
+++ client/src/app/stores/userStore.ts
@@ -1,3 +1,4 @@
+import { access } from "fs";
 import { makeAutoObservable, runInAction } from "mobx";
 import { history } from "../..";
 import agent from "../api/agent";
@@ -6,6 +7,8 @@ import { store } from "./store";
 
 export default class UserStore {
     user: User | null = null;
+    fbAccessToken: string | null = null;
+    fbLoading = false;
     constructor() {
         makeAutoObservable(this)
     }
@@ -56,8 +59,41 @@ export default class UserStore {
     setImage = (image: string) => {
         if (this.user) this.user.image = image;
     }
-    
+
     setDisplayName = (name: string) => {
         if (this.user) this.user.displayName = name;
+    }
+
+    getFacebookLoginStatus = async () => {
+        window.FB.getLoginStatus(response => {
+            if (response.status === 'connected') {
+                this.fbAccessToken = response.authResponse.accessToken;
+            }
+        })
+    }
+
+    facebookLogin = () => {
+        this.fbLoading = true;
+        const apiLogin = (accessToken: string) => {
+            agent.Account.fbLogin(accessToken).then(user => {
+                store.commonStore.setToken(user.token);
+                runInAction(() => {
+                    this.user = user;
+                    this.fbLoading = false;
+                })
+                history.push('/activities');
+            }).catch(error => {
+                console.log(error);
+                runInAction(() => this.fbLoading = false);
+            })
+
+        }
+        if (this.fbAccessToken) {
+            apiLogin(this.fbAccessToken);
+        } else {
+            window.FB.login(response => {
+                agent.Account.fbLogin(response.authResponse.accessToken).then(user => console.log(user));
+            }, { scope: 'public_profile,email' })
+        }
     }
 }
