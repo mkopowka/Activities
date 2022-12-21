a05a21ae937e1a2318de0b9a9857cd4f9ba4de03
Mateusz Kopówka <mkopowka@hotmail.com>
2022-12-21 20:10:02 +0100

    clean code

--- Domain/UserFollowing.cs
+++ Domain/UserFollowing.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Domain
 {
     public class UserFollowing
@@ -14,7 +9,6 @@ namespace Domain
         public string TargetId { get; set; }
 
         public AppUser Target { get; set; }
-
 
     }
 }
